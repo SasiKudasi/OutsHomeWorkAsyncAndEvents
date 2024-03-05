08f2c3f9bdcadb1bd1d8d719ff8094b84d3f09c8
SasiKudasi <sasha.mittal@yandex.ru>
2024-03-05 19:30:04 +0300

    Добавьте файлы проекта.

---
+++ ImageDownloader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW6
+{
+    internal class ImageDownloader
+    {
+        public event Action<string, string>? ImageStarted;
+        public event Action<string, string>? ImageCompleted;
+
+        public string Url { get; set; }
+        public string FileName { get; set; }
+        public ImageDownloader(string url, string fileName)
+        {
+            this.Url = url;
+            this.FileName = fileName;
+        }
+
+        public async Task Download()
+        {
+            FileName += ".jpg";
+            var myWebClient = new WebClient();
+            this.ImageStarted?.Invoke(this.FileName, this.Url);
+            await myWebClient.DownloadFileTaskAsync(Url, FileName);
+            this.ImageCompleted?.Invoke(this.FileName, this.Url);
+        }
+    }
+    class Subsriber
+    {
+        public void ImageStarted(ImageDownloader imageDownloader)
+        {
+            imageDownloader.ImageStarted += (string fileName, string url) =>
+            {
+                Console.WriteLine($"Качаю \"{fileName}\" из \"{url}\" .......\n\n");
+            };
+        }
+
+        public void ImageCompleted(ImageDownloader imageDownloader)
+        {
+            imageDownloader.ImageCompleted += (string fileName, string url) =>
+            {
+                Console.WriteLine($"Успешно скачал \"{fileName}\" из \"{url}\" .......\n\n");
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+
+namespace HW6
+{
+    internal class Program
+    {
+        private List<Task> _tasks = new List<Task>();
+        static void Main(string[] args)
+        {
+            List<Task> tasks = new List<Task>();
+
+            string remoteUri = "https://webneel.com/daily/sites/default/files/images/daily/08-2018/1-nature-photography-spring-season-mumtazshamsee.jpg";
+            string fileName = "bigimage";
+            var download = new ImageDownloader(remoteUri, fileName);
+            var startSender = new Subsriber();
+
+            Task res = null;
+            for (int i = 0; i < 10; i++)
+            {
+                fileName += $"{i}";
+                download = new ImageDownloader(remoteUri, fileName);
+                startSender = new Subsriber(); 
+                startSender.ImageStarted(download);
+                startSender.ImageCompleted(download);
+                res = download.Download();
+                tasks.Add(res);
+
+            }
+            ConsoleKeyInfo key = Console.ReadKey();
+            TaskIsComplited(tasks, key);
+        }
+
+        static void TaskIsComplited(List<Task> values, ConsoleKeyInfo key)
+        {
+            while (true)
+            {
+                key = Console.ReadKey();
+                Console.WriteLine("Нажмите \"A\" для выхода");
+
+                if (key.KeyChar == 'A')
+                {
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    foreach (Task task in values)
+                    {
+                        Console.WriteLine($"Выполнение программы: {task.IsCompleted}");
+                    }
+                }
+            }
+        }
+
+
+    }
+}
